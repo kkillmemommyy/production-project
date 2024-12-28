f004af50d68a27ced8a1aa6ecdbb1ab2eb8d9b70
Kirill <kirilltanae314@gmail.com>
2024-12-28 16:23:04 +0600

    add and configure react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from './components/Counter/Counter';
-import './indes.scss'
+import { Route, Routes } from 'react-router-dom';
+import './indes.scss';
+import { Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Suspense } from 'react';
 
 const App = () => {
   return (
     <div className='app'>
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPageAsync />} />
+          <Route path='/about' element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
