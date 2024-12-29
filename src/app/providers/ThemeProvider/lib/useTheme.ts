f4d62819469ba456d53b620d3845d13a44b02358
Kirill <kirilltanae314@gmail.com>
2024-12-29 22:19:52 +0600

    configure abs paths and fsd refactoring

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,4 +1,4 @@
-import { useCallback, useContext } from 'react';
+import { useContext } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
 
 interface useThemeResult {
