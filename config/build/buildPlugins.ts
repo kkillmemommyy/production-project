8d60708ea0d045e799e8d04dcff75468438f080a
Kirill <kirilltanae314@gmail.com>
2025-01-19 01:01:56 +0600

    add hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,9 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from './types/config';
-import  MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
-  
   return [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
@@ -12,10 +11,11 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css'
+      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev)
-    })
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ];
 };
