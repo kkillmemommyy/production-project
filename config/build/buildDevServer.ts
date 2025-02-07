e2219bb597fb05461d009c3eac3a70ea1cbd15f5
Kirill <kirilltanae314@gmail.com>
2025-02-07 08:22:00 +0600

    add error boundary

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,4 +1,6 @@
-import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import type {
+  Configuration as DevServerConfiguration,
+} from 'webpack-dev-server';
 import { BuildOptions } from './types/config';
 
 export const buildDevServer = (options: BuildOptions): DevServerConfiguration => ({
