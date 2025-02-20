fc755421f748c07f699162784ddf87eaea16d289
Kirill <kirilltanae314@gmail.com>
2025-02-21 01:52:52 +0600

    upgrade sidebar, add btn themes

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,29 +1,48 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { ButtonHTMLAttributes, FC } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import cls from './Button.module.scss';
 
-export enum ThemeButton {
+export enum ButtonTheme {
   CLEAR = 'clear',
   OUTLINE = 'outline',
+  BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted'
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+export enum ButtonSize {
+  M = 'size_m',
+  L = 'size_l',
+  XL = 'size_xl',
+}
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement>{
+  children: ReactNode;
   className?: string;
-  theme?: ThemeButton;
+  theme?: ButtonTheme;
+  square?: boolean;
+  size?: ButtonSize;
 }
 
-export const Button: FC<ButtonProps> = (props) => {
+export const Button = (props: Props) => {
   const {
     className,
     children,
     theme,
+    square,
+    size = ButtonSize.M,
     ...otherProps
   } = props;
+
+  const mods = {
+    [cls[theme]]: true,
+    [cls.square]: square,
+    [cls[size]]: true,
+  };
 
   return (
     <button
       type="button"
-      className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
+      className={classNames(cls.Button, mods, [className])}
       {...otherProps}
     >
       {children}
